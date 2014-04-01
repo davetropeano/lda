c62ddd820feb58e050eb296b11aa9bf0435b5096
davetropeano <davetropeano@yahoo.com>
2014-04-01 10:10:56 -0400

    ported to ld4apps

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/lda/"
-http_images_path = "/lda/images"
+http_path = "/"
+http_images_path = "/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/lda/fonts"
-css_dir = "public/lda/stylesheets"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
